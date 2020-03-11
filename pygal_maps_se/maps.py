61173a2521f2cbc6688de659b5993bbc680d6b50
Johan Vingbäck <johan@siffror.eu>
2020-03-11 14:58:18 +0100

    Added private _plot to lan to allow for color-map plotting

--- pygal_maps_se/maps.py
+++ pygal_maps_se/maps.py
@@ -24,6 +24,9 @@ Swiss cantons map
 from __future__ import division
 from pygal.graph.map import BaseMap
 from pygal._compat import u
+from pygal.etree import etree
+from pygal.graph.graph import Graph
+from pygal.util import alter, cached_property, cut, decorate
 import os
 
 
@@ -73,6 +76,99 @@ class Lan(BaseMap):
     area_prefix = 's'
     kind = 'lan'
     svg_map = LAN_MAP
+
+
+    def _plot(self):
+        """Insert a map in the chart and apply data on it"""
+        map = etree.fromstring(self.svg_map)
+        map.set('width', str(self.view.width))
+        map.set('height', str(self.view.height))
+
+        for i, serie in enumerate(self.series):
+            safe_vals = list(
+                filter(lambda x: x is not None, cut(serie.values, 1))
+            )
+            if not safe_vals:
+                continue
+            min_ = min(safe_vals)
+            max_ = max(safe_vals)
+            for j, (area_code, value) in self.enumerate_values(serie):
+                area_code = self.adapt_code(area_code)
+                if value is None:
+                    continue
+                if max_ == min_:
+                    ratio = 1
+                else:
+                    ratio = .3 + .7 * (value - min_) / (max_ - min_)
+
+                areae = map.findall(
+                    ".//*[@class='%s%s %s map-element']" %
+                    (self.area_prefix, area_code, self.kind)
+                )
+
+                if not areae:
+                    continue
+
+                for area in areae:
+                    cls = area.get('class', '').split(' ')
+                    cls.append('color-%d' % i)
+                    cls.append('serie-%d' % i)
+                    cls.append('series')
+                    area.set('class', ' '.join(cls))
+                    if getattr(self, 'use_cmap', False):
+                        if getattr(self, 'cmap', None) is None:
+                            self.cmap = {min_: (16,255,16), max_: (255,16,16)}
+                        if value in self.cmap.keys():
+                            mcol = self.cmap[value]
+                        else:
+                            ks = self.cmap.keys()
+                            if value < min(ks):
+                                mcol = self.cmap[min(ks)]
+                            elif value > max(ks):
+                                mcol = self.cmap[max(ks)]
+                            else:
+                                kl = [x for x in ks if x < value][-1]
+                                kh = [x for x in ks if x > value][ 0]
+                                lc = self.cmap[ kl ]
+                                hc = self.cmap[ kh  ]
+                                rat = (value-kl)/(kh-kl)
+                                mcol = (
+                                    int(lc[0]+(hc[0]-lc[0])*rat),
+                                    int(lc[1]+(hc[1]-lc[1])*rat),
+                                    int(lc[2]+(hc[2]-lc[2])*rat)
+                                )
+
+                        mcolor = "#%02x%02x%02x" % mcol
+                        area.set('style', 'fill-opacity: %f; fill: %s' % (0.7, mcolor))
+                    else:
+                        area.set('style', 'fill-opacity: %f' % ratio)
+                    metadata = serie.metadata.get(j)
+
+                    if metadata:
+                        node = decorate(self.svg, area, metadata)
+                        if node != area:
+                            area.remove(node)
+                            for g in map:
+                                if area not in g:
+                                    continue
+                                index = list(g).index(area)
+                                g.remove(area)
+                                node.append(area)
+                                g.insert(index, node)
+
+                    for node in area:
+                        cls = node.get('class', '').split(' ')
+                        cls.append('reactive')
+                        cls.append('tooltip-trigger')
+                        cls.append('map-area')
+                        node.set('class', ' '.join(cls))
+                        alter(node, metadata)
+
+                    val = self._format(serie, j)
+                    self._tooltip_data(area, val, 0, 0, 'auto')
+
+        self.nodes['plot'].append(map)
+
 
 class VagRegion(BaseMap):
     """ Swedish Vägregion map """
